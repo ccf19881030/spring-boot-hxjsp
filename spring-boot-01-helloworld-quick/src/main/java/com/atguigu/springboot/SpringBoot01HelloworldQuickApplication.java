5d974f8489d26383e836220ef7d5f8d9e80d5624
Rinko <qw85123163@163.com>
2019-03-05 13:16:54 +0800

    使用快速向导创建SpringBoot项目

--- spring-boot-01-helloworld-quick/src/main/java/com/atguigu/springboot/SpringBoot01HelloworldQuickApplication.java
+++ spring-boot-01-helloworld-quick/src/main/java/com/atguigu/springboot/SpringBoot01HelloworldQuickApplication.java
@@ -3,6 +3,9 @@ package com.atguigu.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 使用快速向导创建的项目会默认生成需要的目录结构
+ */
 @SpringBootApplication
 public class SpringBoot01HelloworldQuickApplication {
 
