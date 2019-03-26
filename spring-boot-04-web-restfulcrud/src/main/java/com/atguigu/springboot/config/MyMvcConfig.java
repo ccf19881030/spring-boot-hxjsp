745b1cc397834fb515fcf638be7115df6457792d
Rinko <qw85123163@163.com>
2019-03-26 11:06:48 +0800

    web开发 - 模拟登陆、登陆拦截器（公共页面、静态资源路径需排除否则将被拦截）

--- spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,18 +1,16 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.LoginHandlerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
 import java.util.Locale;
 
 /**
@@ -27,35 +25,43 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         System.out.println("注册拦截器");
-        HandlerInterceptor myInterceptor = new HandlerInterceptor() {
-            @Override
-            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-                System.out.println("my interceptor preHandle...");
-                //通过返回true
-                return true;
-            }
+//        HandlerInterceptor myInterceptor = new HandlerInterceptor() {
+//            @Override
+//            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//                System.out.println("my interceptor preHandle...");
+//                //通过返回true
+//                return true;
+//            }
+//
+//            @Override
+//            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//                System.out.println("my interceptor postHandle...");
+//            }
+//
+//            @Override
+//            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+//                System.out.println("my interceptor afterCompletion...");
+//            }
+//        };
+//        registry.addInterceptor(myInterceptor).excludePathPatterns(Arrays.asList("/public/**", "/static/**")); //排除静态资源
 
-            @Override
-            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-                System.out.println("my interceptor postHandle...");
-            }
-
-            @Override
-            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-                System.out.println("my interceptor afterCompletion...");
-            }
-        };
-        registry.addInterceptor(myInterceptor).excludePathPatterns(Arrays.asList("/public/**", "/static/**")); //排除静态资源
+        //静态资源；  *.css , *.js
+        //1.5版本SpringBoot已经做好了静态资源映射
+        //2.0版本要自己排除
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**") //拦截全部路径请求
+                .excludePathPatterns("/index.html", "/", "/user/login", "/public/**", "/asserts/**","/webjars/**");        //手动排除静态资源路径
     }
 
     /**
-     * 映射静态资源链接
+     * 映射静态资源链接 视图映射
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/index2").setViewName("index2");
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/index.html").setViewName("login");
+        registry.addViewController("/main.html").setViewName("dashboard");  //首页
     }
 
     /**
